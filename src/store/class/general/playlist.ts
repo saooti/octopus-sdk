c38ea9a4b74505eb590d6bd54ef5f12307b1f28a
CamilleSaulnier <camille@saooti.com>
2022-08-30 11:13:33 +0200

    PlaylistItem

--- src/store/class/general/playlist.ts
+++ src/store/class/general/playlist.ts
@@ -1,13 +1,33 @@
 import { Organisation } from '../general/organisation';
 import { Person } from '../user/person';
+import { FetchParam } from './fetchParam';
+
 
 export interface Playlist {
   imageUrl?: string;
   description: string;
   organisation?: Organisation;
   playlistId: number;
-  podcasts: Array<number | undefined>;
+  podcasts?: Array<number | undefined>;
   score: number;
   title: string;
+  type: string;
+  samplingViews?: Array<PlaylistRule>;
   publisher?: Person;
+}
+
+export interface PlaylistRule {
+  maxSize: number,
+  podcastId?: number,
+  query?: FetchParam,
+  type?:string,
+  title?: string,
+}
+
+export function emptyPlaylistRule(): PlaylistRule{
+  return {
+    maxSize: 1,
+    query: {},
+    title: undefined,
+  }
 }
