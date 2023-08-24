2ea28afeb4fd5ee841b36adb374b36917bb5d0bd
CamilleSaulnier <camille@saooti.com>
2023-08-24 15:15:27 +0200

    Accordion error

--- src/components/misc/Accordion.vue
+++ src/components/misc/Accordion.vue
@@ -1,65 +1,60 @@
 <template>
-  <div
-    class="octopus-accordion my-2"
-  >
-    <button
-      :id="'accordion-'+idComposer"
-      class="btn-transparent bg-white w-100 p-2 text-start d-flex align-items-center"
-      :class="isOpen?'really-light-primary-bg':''"
-      @click="isOpen=!isOpen"
-    >
-      <span
-        v-if="icon"
-        class="img-accordion text-primary"
-        :class="icon"
-      />
-      <img
-        v-if="imageUrl"
-        width="30"
-        height="30"
-        class="img-accordion"
-        :src="imageUrl"
-        :alt="title"
+  <div class="my-2" :class="displayAccordion ? 'octopus-accordion' : ''">
+    <template v-if="displayAccordion">
+      <button
+        :id="'accordion-' + idComposer"
+        class="btn-transparent bg-white w-100 p-2 text-start d-flex align-items-center"
+        :class="isOpen ? 'really-light-primary-bg' : ''"
+        @click="isOpen = !isOpen"
       >
-      <span class="flex-grow-1">{{ title }}</span>
-      <span :class="isOpen?'saooti-up': 'saooti-down'" />
-    </button>
-    <div
-      v-show="isOpen"
-      class="body p-2"
-    >
-      <slot />
-    </div>
+        <span v-if="icon" class="img-accordion text-primary" :class="icon" />
+        <img
+          v-if="imageUrl"
+          width="30"
+          height="30"
+          class="img-accordion"
+          :src="imageUrl"
+          :alt="title"
+        />
+        <span class="flex-grow-1">{{ title }}</span>
+        <span :class="isOpen ? 'saooti-up' : 'saooti-down'" />
+      </button>
+      <div v-show="isOpen" class="body p-2">
+        <slot />
+      </div>
+    </template>
+    <slot v-else />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 export default defineComponent({
-  name: "Accordion",
-	props: {
-    title: { default: '', type: String },
-    idComposer: { default: '', type: String },
-    icon:{default: undefined, type: String },
-    imageUrl:{default: undefined, type: String },
+  name: "ClassicAccordion",
+  props: {
+    title: { default: "", type: String },
+    idComposer: { default: "", type: String },
+    icon: { default: undefined, type: String },
+    imageUrl: { default: undefined, type: String },
+    displayAccordion: { default: true, type: Boolean },
   },
-	emits:['open'],
-  data () {
+  emits: ["open"],
+  data() {
     return {
-			isOpen: false as boolean,
-    }
+      isOpen: false as boolean,
+    };
   },
-  watch:{
-    isOpen(){
-      this.$emit('open');
-    }
-  }
+  watch: {
+    isOpen() {
+      this.$emit("open");
+    },
+  },
 });
 </script>
 <style lang="scss">
-.octopus-accordion{
-	border: 1px solid #ccc;
-  >button{
+.octopus-accordion {
+  border: 1px solid #ccc;
+  > button {
     height: 50px;
   }
   .img-accordion {
@@ -71,7 +66,7 @@ export default defineComponent({
     justify-content: center;
     align-items: center;
   }
-  .body{
+  .body {
     border-top: 1px solid #ccc;
   }
 }
