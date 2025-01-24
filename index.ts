a4e221caeccea90a2bf967ae9bb6a03641c5736f
CamilleSaulnier <camille@saooti.com>
2025-01-24 09:15:37 +0100

    Exposition de la page logout

--- index.ts
+++ index.ts
@@ -17,6 +17,7 @@ export const getRadio = () => import("./src/components/pages/RadioPage.vue");
 export const getVideo = () => import("./src/components/pages/VideoPage.vue");
 export const getMapPage= () => import("./src/components/pages/MapPage.vue");
 export const getPageNotFound= () => import("./src/components/pages/PageNotFound.vue");
+export const getPageLogout= () => import("./src/components/pages/PageLogout.vue");
 
 //Misc
 export const getAccordion = () => import("./src/components/misc/ClassicAccordion.vue");
