9a9a624f32f4077b83afc5d00e58089b4544e10a
CamilleSaulnier <camille@saooti.com>
2025-03-19 16:56:57 +0100

    Pb popover

--- src/components/misc/ClassicPopover.vue
+++ src/components/misc/ClassicPopover.vue
@@ -245,7 +245,8 @@ export default defineComponent({
         return;
       }
       //Exception timepicker in popover
-      if (e?.target?.classList?.contains("dp__time_display")) {
+      var result = Array.from(e?.target?.classList ?? []).findIndex((val) => { return val.startsWith("dp__");});
+      if (-1!==result) {
         return;
       }
       if (!e.relatedTarget) {
