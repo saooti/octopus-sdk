007d61dd8e2b81dc70dddbda990ab20a5e08c256
Julien Guguin <julien.guguin@saooti.com>
2026-09-22 17:17:52 +0200

    Début tests pour un sélecteur de portée

---
+++ src/components/misc/RightsScopeSelector.vue
@@ -0,0 +1,89 @@
+<template>
+    <div
+        v-if="subOrganisations.length > 0"
+        class="d-flex flex-column ms-4"
+    >
+        <span class="label mb-1">Filtrage du contenu ({{ rubriquage?.title }})</span>
+        <div class="d-flex selected">
+            <span class="me-2">{{ selectedSubOrganisation?.name ?? 'Aucun filtrage' }}</span>
+            <SwapIcon @click="$emit('change')" />
+            <ClearIcon v-if="selectedSubOrganisation" />
+        </div>
+    </div>
+    <OctopusSelect
+        id="header-scope-selector"
+        :value="selectedSubOrganisation ?? undefined"
+        :options="subOrganisations"
+        option-key="id"
+        option-label="name"
+        @update:value="selectSubOrganisation"
+    />
+</template>
+
+<script setup lang="ts">
+import { useAuthStore } from "../../stores/AuthStore";
+import { ref, toRef, watch } from "vue";
+import { useSubOrganisations } from "../composable/useSubOrganisations";
+import OctopusSelect from "../form/OctopusSelect.vue";
+import { Rubriquage } from "@/stores/class/rubrique/rubriquage";
+import { rubriquesApi } from "@/api";
+
+import SwapIcon from "vue-material-design-icons/SwapHorizontalCircleOutline.vue";
+import ClearIcon from "vue-material-design-icons/CloseCircleOutline.vue";
+
+//Composables
+const authStore = useAuthStore();
+const authOrgaId = toRef(authStore, 'authOrgaId');
+const rubriquage = ref<Rubriquage|null>(null);
+const {
+    selectSubOrganisation,
+    selectedSubOrganisation,
+    subOrganisations
+} = useSubOrganisations(authOrgaId);
+
+defineEmits<{
+    (e: 'change'): void;
+}>();
+
+watch(authOrgaId, async () => {
+    rubriquage.value = await rubriquesApi.findRestrictiveRubriquage(authOrgaId.value);
+}, { immediate: true });
+</script>
+
+<style scoped lang="scss">
+#header-scope-selector:deep(.octopus-select-field) {
+    background: transparent;
+    border-color: rgba(255, 255, 255, 0.1);
+    margin-left: 200px;
+
+    .octopus-select-value {
+        color: var(--octopus-secondary);
+        font-weight: bold;
+        align-content: center;
+    }
+
+    input {
+        color: white;
+    }
+
+    button {
+        color: rgba(255, 255, 255, 0.3);
+    }
+}
+
+.selected {
+    color: var(--octopus-secondary);
+    font-weight: bold;
+    align-content: center;
+    align-items: center;
+}
+
+.label {
+    color: rgba(255, 255, 255, 0.3);
+    font-weight: 600;
+    font-style: italic;
+    font-size: 14px;
+    white-space: nowrap;
+    overflow-y: visible;
+}
+</style>
--- src/components/misc/TopBarMainContent.vue
+++ src/components/misc/TopBarMainContent.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="top-bar-grid header-content" :class="{ scrolled: titleIsDisplayed }">
-        <div class="d-flex">
+        <div class="d-flex align-items-center">
             <!-- Logo to the left -->
             <router-link
-                class="top-bar-logo d-flex align-items-center"
+                class="top-bar-logo d-block"
                 :to="{
                     name: 'home',
                     query: getQueriesRouter('home'),
@@ -30,6 +30,9 @@
                     :class="generalStore.platformEducation ? 'education-logo' : ''"
                 >
             </router-link>
+
+            <RightsScopeMenu v-model:open="menuOpen" />
+            <RightsScopeSelector class="ms-4" @change="menuOpen = true" />
         </div>
 
         <h1 v-if="titleIsDisplayed" class="text-truncate m-0 align-self-center">
@@ -173,9 +176,11 @@ import {useImageProxy} from "../composable/useImageProxy";
 import { useFilterStore } from "../../stores/FilterStore";
 import { useAuthStore } from "../../stores/AuthStore";
 import ClassicPopover from "../misc/ClassicPopover.vue";
-import {  defineAsyncComponent, computed } from "vue";
+import { defineAsyncComponent, computed, ref } from "vue";
 import { useGeneralStore } from "../../stores/GeneralStore";
 import { useI18n } from "vue-i18n";
+import RightsScopeSelector from "./RightsScopeSelector.vue";
+import RightsScopeMenu from "./RightsScopeMenu.vue"; 
 const MobileMenu = defineAsyncComponent(() => import("./MobileMenu.vue"));
 
 export interface TopBarMainContentOptions {
@@ -202,6 +207,8 @@ const { rubriqueQueryParam } = useRubriquesFilterComputed();
 const authStore = useAuthStore();
 const generalStore = useGeneralStore();
 const filterStore = useFilterStore();
+
+const menuOpen = ref(false);
 
 //Computed
 const mobileMenuDisplay = computed(() => props.isPhone || inContentDisplayPage.value);
@@ -280,7 +287,7 @@ function getQueriesRouter(routeName: string) {
 .top-bar-grid {
     display: grid;
     grid-template-columns: 1fr 5fr;
-    padding: 0 1rem;
+    padding: 0 1rem 0 0.5rem;
 
     &.scrolled {
         grid-template-columns: 1fr 2fr 1fr;
